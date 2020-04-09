02ddefb8efe3b17be4f6e1da336663df15574e4a
gugova <63418622+gugova@users.noreply.github.com>
2020-04-09 20:48:15 +0300

    Project

---
+++ CommandPattern/src/Command/DeleteCommand.java
@@ -0,0 +1,16 @@
+package Command;
+
+public class DeleteCommand implements Command{
+	Database database;
+	
+	public DeleteCommand(Database database) {
+		this.database = database;
+		}
+	
+	@Override
+	public void execute() {
+		database.delete();
+		
+	}
+
+}
